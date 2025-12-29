a4eb86895cc07901ef22a6a3a2555869924340e4
abhayjaiswal8688 <145705056+abhayjaiswal8688@users.noreply.github.com>
2025-12-29 20:40:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const hf = new InferenceClient(process.env.HF_TOKEN);
 
-app.use(cors({
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
@@ -143,7 +139,6 @@ app.get('/api/users/me', authMiddleware, async (req, res) => {
 // Startup
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('MongoDB connected.');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => console.error(err));
 // Add this simple route to test if the server is up
 app.get('/', (req, res) => {
